3df5e4a0e1f3e5ec0ef242ef22679a5d2d8304fb
mrsicsasa <sasamrsic20@gmail.com>
2024-02-17 20:08:01 +0100

    added IF

--- parser/parser.go
+++ parser/parser.go
@@ -50,6 +50,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.TRUE,p.parseBoolean)
 	p.registerPrefix(token.FALSE,p.parseBoolean)
 	p.registerPrefix(token.LPAREN,p.parseGroundExpression)
+	p.registerPrefix(token.IF,p.parseIfExpression)
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
     p.registerInfix(token.PLUS, p.parseInfixExpression)
     p.registerInfix(token.MINUS, p.parseInfixExpression)
@@ -239,4 +240,41 @@ func (p *Parser)parseGroundExpression()ast.Expression{
 		return nil
 	}
 	return exp
+}
+func (p *Parser)parseIfExpression()ast.Expression{
+	expression:=&ast.IfExpression{Token:p.curToken}
+	if !p.expectPeek(token.LPAREN){
+		return nil
+	}
+	p.NextToken()
+	expression.Condition=p.ParseExpression(LOWEST)
+	if !p.expectPeek(token.RPAREN){
+		return nil
+	}
+	if !p.expectPeek(token.LBRACE){
+		return nil
+	}
+	expression.Consequence=p.parseBlockStatement()
+	if p.peekTokenIs(token.ELSE){
+		p.NextToken()
+	
+	if !p.expectPeek(token.LBRACE){
+		return nil
+	}
+	expression.Alternative=p.parseBlockStatement()
+	}
+	return expression
+}
+func (p *Parser)parseBlockStatement()*ast.BlockStatement{
+	block:=&ast.BlockStatement{Token:p.curToken}
+	block.Statements=[]ast.Statement{}
+	p.NextToken()
+	for !p.curTokenIs(token.RBRACE) && !p.curTokenIs(token.EOF){
+		stmt:=p.parseStatement()
+		if stmt!=nil{
+			block.Statements=append(block.Statements, stmt)
+		}
+		p.NextToken()
+	}
+	return block
 }

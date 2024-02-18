e29d9a2397538c20e7af712aee3a2ab9b7f8840c
mrsicsasa <sasamrsic20@gmail.com>
2024-02-18 19:24:23 +0100

    added parsing for functions

--- parser/parser.go
+++ parser/parser.go
@@ -1,3 +1,4 @@
+/* trunk-ignore-all(gofmt) */
 package parser
 
 import (
@@ -51,6 +52,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.FALSE,p.parseBoolean)
 	p.registerPrefix(token.LPAREN,p.parseGroundExpression)
 	p.registerPrefix(token.IF,p.parseIfExpression)
+	p.registerPrefix(token.FUNCTION,p.parseFunctionLiteral)
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
     p.registerInfix(token.PLUS, p.parseInfixExpression)
     p.registerInfix(token.MINUS, p.parseInfixExpression)
@@ -277,4 +279,41 @@ func (p *Parser)parseBlockStatement()*ast.BlockStatement{
 		p.NextToken()
 	}
 	return block
+}
+func (p *Parser) parseFunctionLiteral() ast.Expression {
+	lit := &ast.FunctionLiteral{Token: p.curToken}
+
+	if !p.expectPeek(token.LPAREN) {
+		return nil
+	}
+
+	lit.Parameters = p.parseFunctionParameters()
+
+	if !p.expectPeek(token.LBRACE) {
+		return nil
+	}
+
+	lit.Body = p.parseBlockStatement()
+
+	return lit
+}
+func (p *Parser)parseFunctionParameters()[]*ast.Identifier{
+	identifiers:=[]*ast.Identifier{}
+	if p.peekTokenIs(token.RPAREN){
+		p.NextToken()
+		return identifiers
+	}
+	p.NextToken()
+	ident:=&ast.Identifier{Token:p.curToken,Value: p.curToken.Literal}
+	identifiers=append(identifiers, ident)
+	for p.peekTokenIs(token.COMMA){
+		p.NextToken()
+		p.NextToken()
+		ident:=&ast.Identifier{Token:p.curToken,Value: p.curToken.Literal}
+		identifiers=append(identifiers, ident)
+	}
+	if !p.expectPeek(token.RPAREN){
+		return nil
+	}
+	return identifiers
 }
